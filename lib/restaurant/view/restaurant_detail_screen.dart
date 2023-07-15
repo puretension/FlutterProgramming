42e840d5b7b35ed653549e200358b166108ead59
puretension <rlrlfhtm5@gmail.com>
2023-07-15 23:55:57 +0900

    Pagination,DataModeling,detailProductScreen

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -10,25 +10,82 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-              image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-              name: '불타는 떡볶이',
-              tags: ['떡볶이','맛있음','치즈'],
-              ratingsCount: 100,
-              deliveryFee: 30,
-              deliveryTime: 3000,
-              ratings: 4.76,
-              isDetail: true,
-            detail: '맛있는 떡볶이',
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          ),
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+
+      // Column(
+      //   children: [
+      //     RestaurantCard(
+      //         image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+      //         name: '불타는 떡볶이',
+      //         tags: ['떡볶이','맛있음','치즈'],
+      //         ratingsCount: 100,
+      //         deliveryFee: 30,
+      //         deliveryTime: 3000,
+      //         ratings: 4.76,
+      //         isDetail: true,
+      //       detail: '맛있는 떡볶이',
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      //       child: ProductCard(),
+      //     ),
+      //   ],
+      // ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop(){
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: ['떡볶이','맛있음','치즈'],
+        ratingsCount: 100,
+        deliveryFee: 30,
+        deliveryTime: 3000,
+        ratings: 4.76,
+        isDetail: true,
+        detail: '맛있는 떡볶이',
       ),
     );
   }
+
+  SliverPadding renderLabel(){
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
+  renderProducts(){
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+            (context, index){
+              return Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: ProductCard(),
+              );
+            },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
 }
